6bab8ced0b0dde4293e92740e6e1dd05d7554d73
adavies312 <a.m.davies1020@gmail.com>
2018-05-24 21:14:29 -0500

    Friend Finder

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,7 +1,27 @@
 var friends = require('../data/friends.js');
 
 module.exports = function (app){
-    app.get('/api/data/friends', function(req, res){
+    app.get('/api/friends', function(req, res){
         res.json(friends);
-    })
+    });
+
+    app.post('/api/friends'), function(req, res){
+        //make post route work
+        //add up scores for each person
+        // a for loop for friends
+        // get user data 
+        // variable for difference in scores
+        // Determine the best match using difference
+        var difference = 0;
+        for (var i=0; i< friends.length; i++){
+            console.log(friends[i].name);
+
+            for (var j=0; j<friends[i].score[j]; j++){
+                difference += Math.abs(parseInt(score[j]) - parseInt(friends[i].score[j]))
+                console.log(difference + "Loop being hit");
+            }
+        }
+
+    }
+
 }
